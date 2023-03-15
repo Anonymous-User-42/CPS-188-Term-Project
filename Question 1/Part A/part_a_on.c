be63541a43718efaca3f0cfab24fa8cb61471fc9
Sayeed Ahamad <mohammedsayeedahamad@gmail.com>
2023-03-15 18:41:03 -0400

    Q1 Part A file open & close UDF

--- Question 1/Part A/part_a_on.c
+++ Question 1/Part A/part_a_on.c
@@ -1,4 +1,26 @@
 
 
+#include <stdio.h>
+#include <stdlib>
+#include <math.h>
 
+void main(void)
+{
+    FILE* f = file_o();
+
+    file_c(f);
+}
+
+void file_o(void)   /*  UDF for file opening    */
+{
+    FILE* file; /*  Initializing file storage variable  */
+    file = fopen("statscan_diabetes.csv", "r"); /*  Defining the address path of file to be opened and read    */
+    fscanf(file, "%d", &temp)   /*  Scanning the defined file and storing it at address of Var(temp)    */
+    return file;    /*  Returning the file type variable    */
+}
+
+void file_c(FILE* file) /*  UDF for file closing    */
+{
+    fclose(file);   /*  Closing the already opened file */
+}
 
