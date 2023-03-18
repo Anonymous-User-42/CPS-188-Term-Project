6f3101e2dd8b4327c11782065cfc937f504cacc7
Anonymous-User-42 <mohammedsayeedahamad@gmail.com>
2023-03-18 19:17:40 -0400

    Amending Illigal Syntactic issues from previous commit

--- Question 1/Part A/part_a_on.c
+++ Question 1/Part A/part_a_on.c
@@ -4,6 +4,9 @@
 #include <stdlib.h>
 #include <math.h>
 
+FILE* file_o(void);
+void file_c(FILE* file);
+
 void main(void)
 {
     FILE* f = file_o();
@@ -11,11 +14,12 @@ void main(void)
     file_c(f);
 }
 
-void file_o(void)   /*  UDF for file opening    */
+FILE* file_o(void)   /*  UDF for file opening    */
 {
+    char temp[100];
     FILE* file; /*  Initializing file storage variable  */
     file = fopen("statscan_diabetes.csv", "r"); /*  Defining the address path of file to be opened and read    */
-    fscanf(file, "%d", &temp)   /*  Scanning the defined file and storing it at address of Var(temp)    */
+    fscanf(file, "%d", temp);  /*  Scanning the defined file and storing it at address of Var(temp)    */
     return file;    /*  Returning the file type variable    */
 }
 
