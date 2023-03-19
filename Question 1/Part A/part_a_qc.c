e1d4f00f05ab27811b7339d663d0b0539dd7d0b4
Qurrat ul Ain <127075125+Qurratulain2@users.noreply.github.com>
2023-03-19 01:15:34 -0400

    Update part_a_qc.c

--- Question 1/Part A/part_a_qc.c
+++ Question 1/Part A/part_a_qc.c
@@ -1,4 +1,28 @@
+#include <stdio.h>
 
+int main(void) {
+    char c;
+    FILE* in;
+    in = fopen("statscan_diab1etes.txt", "r");
+    if (in != NULL)    
+     {
+          while ((c = fgetc(in)) != EOF) {
+            putchar(c);
+        }
+        fclose(in);
+    } else {
+        printf("Error opening file.\n");
+    }
+    return 0;
+}
+
+//
+// Open the file using fopen() function.
+// Search for  Quebec province 
+// Use a loop to read each line of the file.
+// If = Quebec, extract the value in the second column and add it to a running total.
+// After reading all lines, divide the running total by the number of rows that had Quebec as the first column.
+// Print the average.
 
 
 
