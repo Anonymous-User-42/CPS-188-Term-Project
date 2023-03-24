3dc28fb085581658af8235c4a1fe6c1820763ef2
ThatTreeGuy25 <127777351+ThatTreeGuy25@users.noreply.github.com>
2023-03-24 03:53:41 -0400

    Update part_a_ab.c

--- Question 1/Part A/part_a_ab.c
+++ Question 1/Part A/part_a_ab.c
@@ -1,32 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main ()
-{
-	double average; 
-printf ("\t\t\t   Males with diabetes in Alberta (Percentages)\n\n");
-printf ("\t\t2015\t 2016\t 2017\t 2018\t 2019\t 2020\t 2021\n");
-printf ("35 - 49\t\t5.4\t 6\t 3.2\t 3.7\t 7.6\t 8.7\t 2.4\n");
-printf ("50 - 64\t\t9.5\t 10.7\t 16.1\t 13\t 12.4\t 13.1\t 13.9\n");
-printf ("65+\t\t15.8\t 15.8\t 22.4\t 23.6\t 22.8\t 21.2\t 20.6\n\n"); 
+int main() {
+	
+	char line [1000]; // Create an array to store lines read from file 
+	char *column; // Pointer to store columns 
+	int start_row = 128; // The first row Alberta begins at 
+	int last_row = 169; // The last Alberta Row 
+	int num_columns[] = {1, 2, 5, 14}; // Column numbers to print 	
+	int current_row = 0; // current row being read
+	int current_column = 0; //current column being read 
+	int num_cols = sizeof(num_columns) / sizeof(num_columns[0]); //Find number of columns in the array
+	
+	FILE *fp; 
+	
+	fp = fopen ("statscan_diabetes.csv", "r"); //Open the file to be read
+	
+	if (fp == NULL) { //Check if file can be opened, if it cannot display Cannot Open File
+		printf ("Cannot Open the File");
+		exit(0);
+	}
 
-printf ("\t\t\t   Females with Diabetes in Alberta (Percentages)\n\n");
-printf ("\t\t2015\t 2016\t 2017\t 2018\t 2019\t 2020\t 2021\n");
-printf ("35 - 49\t\tN/A\t 2.2\t 3.5\t 3.6\t 3.8\t N/A\t 3.4\n");
-printf ("50 - 64\t\t5.1\t 6.2\t 11.6\t 10.9\t 7.5\t 7\t 7\n");
-printf ("65+\t\t10.8\t 17.7\t 15\t 11.3\t 13.9\t 14.4\t 11.6");
+	while (fgets(line, 1000, fp) != NULL) { // read all lines and print required rows/columns
+		current_row++;
+		if (current_row >= start_row && current_row <= last_row) {
+			current_column = 0;
+			column = strtok(line, ",");
+			while (column != NULL) {
+				current_column++;
+				for (int i = 0; i < num_cols; i++) { 
+					if (current_column == num_columns[i]) {
+						printf ("%s,", column);
+					}
+				}
+				column = strtok(NULL, ",");
+			}
+			printf ("\n");
+		}
+	}
 
-average = (5.4 + 9.5 + 15.8 + 6 + 10.7 + 15.8 + 3.2 + 16.1 + 22.4 
-+ 3.7 + 13 + 23.6 + 7.6 + 12.4 + 22.8 + 8.7 + 13.1 + 21.2 + 2.4 + 13.9 
-+ 20.6 + 5.1 + 10.8 + 2.2 + 6.2 + 17.7 + 3.5 + 11.6 + 15 + 3.6 + 10.9 
-+ 11.3 +3.8 + 7.5 + 13.9 + 7 + 14.4) / 40;
-printf ("\n\nThe average percentage of people in Alberta with Diabetes is: %.2lf%%\n", average);
+		
+	fclose (fp);
 
-
-
-
-return (0);
-
-;
+	return(0);
 }
 
 
