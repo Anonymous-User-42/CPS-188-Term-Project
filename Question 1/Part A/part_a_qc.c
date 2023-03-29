9e00af613380992c120fb99cbb08d8bd4331db4e
Qurrat ul Ain <127075125+Qurratulain2@users.noreply.github.com>
2023-03-29 00:25:01 -0400

    Update part_a_qc.c

--- Question 1/Part A/part_a_qc.c
+++ Question 1/Part A/part_a_qc.c
@@ -1,29 +1,64 @@
-#include <stdio.h>
 
-int main(void) {
-    char c;
-    FILE* in;
-    in = fopen("statscan_diab1etes.txt", "r");
-    if (in != NULL)    
-     {
-          while ((c = fgetc(in)) != EOF)  //reads characters from a file until the end of the file is reached, storing each character in the variable 'c'
-          {
-            putchar(c);
-        }
-        fclose(in);
-    } else {
-        printf("Error opening file.\n");
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    FILE *fp;
+    int year;
+    double ontario[5], quebec[5], alberta[5], britishColumbia[5];
+    double ontarioAvg, quebecAvg, albertaAvg, britishColumbiaAvg;
+    double overallAvg;
+    int i;
+
+    fp = fopen("statscan_diab1etes.txt", "r");
+    if (fp == NULL) {
+        printf("Error: Could not open the input file");
+        exit(0);
     }
+
+    i = 0;
+    while (!feof(fp) && i < 5) {
+        char line[100];
+        fgets(line, 100, fp);
+        sscanf(line, "%d,%lf,%lf,%lf,%lf\n", &year, &ontario[i], &quebec[i], &alberta[i], &britishColumbia[i]);
+        i++;
+    }
+    fclose(fp);
+
+    ontarioAvg = quebecAvg = albertaAvg = britishColumbiaAvg = 0;
+    int countOntario = 0, countQuebec = 0, countAlberta = 0, countBritishColumbia = 0;
+    for (i = 0; i < 5; i++) {
+        if (ontario[i] != -1) { // ignore missing data
+            ontarioAvg += ontario[i];
+            countOntario++;
+        }
+        if (quebec[i] != -1) {
+            quebecAvg += quebec[i];
+            countQuebec++;
+        }
+        if (alberta[i] != -1) {
+            albertaAvg += alberta[i];
+            countAlberta++;
+        }
+        if (britishColumbia[i] != -1) {
+            britishColumbiaAvg += britishColumbia[i];
+            countBritishColumbia++;
+        }
+    }
+    ontarioAvg /= countOntario;
+    quebecAvg /= countQuebec;
+    albertaAvg /= countAlberta;
+    britishColumbiaAvg /= countBritishColumbia;
+    overallAvg = (ontarioAvg + quebecAvg + albertaAvg + britishColumbiaAvg) / 4;
+
+    printf("Ontario Average = %.2lf\n", ontarioAvg);
+    printf("Quebec Average = %.2lf\n", quebecAvg);
+    printf("Alberta Average = %.2lf\n", albertaAvg);
+    printf("British Columbia Average = %.2lf\n", britishColumbiaAvg);
+    printf("Overall Average = %.2lf\n", overallAvg);
+
     return 0;
 }
-
-//
-// Open the file using fopen() function.
-// Search for  Quebec province 
-// Use a loop to read each line of the file.
-// If = Quebec, extract the value in the second column and add it to a running total.
-// After reading all lines, divide the running total by the number of rows that had Quebec as the first column.
-// Print the average.
-
 
 
