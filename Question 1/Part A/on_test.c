342548ba013aaed8e0f5d553c6c4af0b29eeb4e2
Sayeed Ahamad <88463176+Anonymous-User-42@users.noreply.github.com>
2023-03-29 16:06:15 +0000

    Attempted Tokenization for CSV though for loop strcpy

--- Question 1/Part A/on_test.c
+++ Question 1/Part A/on_test.c
@@ -1,37 +1,62 @@
 
 
-#include<stdio.h>
-#include<string.h>
-#include<stdbool.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
 
-#define MAXCHAR 1000
+#define ARRAY_SIZE 500
+#define LINE_SIZE 250
 
-int main(){
+typedef struct {
+  char year[10];
+  char province[35];
+  char age_group[15];
+  char sex[10];
+  char values[5];
+} datatypes;
 
-    FILE *fp;
-    char row[MAXCHAR];
-    char *token;
+void main(void) {
+  FILE *fp;
+  char row[ARRAY_SIZE];
+  char *token;
 
-    fp = fopen("statscan_diabetes.csv","r");
+  fp = fopen("statscan_diabetes.csv", "r");
 
+  datatypes data_points[ARRAY_SIZE];
 
-    while (feof(fp) != true)
-    {
-        fgets(row, MAXCHAR, fp);
-        printf("Row: %s", row);
+  int line_count = 0;
 
-        token = strtok(row, ",");
-
-        while(token != NULL)
-        {
-            printf("Token: %s\n", token);
-            token = strtok(NULL, ",");
-        }
-
+  while (feof(fp) != true) {
+    if (line_count == 0) {
+      continue;
+    } else {
+      fgets(row, LINE_SIZE, fp);
     }
-    
 
-    return 0;
+    token = strtok(row, ",");
 
+    for (int count; token != NULL; token = strtok(NULL, ","), count++) {
+      if (count == 0) {
+        strcpy(data_points[count].year, token);
+      };
+      if (count == 1) {
+        strcpy(data_points[count].province, token);
+      };
+      if (count == 3) {
+        strcpy(data_points[count].age_group, token);
+      };
+      if (count == 4) {
+        strcpy(data_points[count].sex, token);
+      };
+      if (count == 13) {
+        strcpy(data_points[count].values, token);
+      };
+      token = strtok(NULL, ",");
+    }
+    line_count++;
+  }
+
+  for (int i; i <= ARRAY_SIZE; i++) {
+    printf("%s", data_points[i].province);
+  }
 }
-
