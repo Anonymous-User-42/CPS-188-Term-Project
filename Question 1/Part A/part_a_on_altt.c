41fc9a70aa80746df383be28ad0e326aa9c9467f
Anonymous-User-42 <mohammedsayeedahamad@gmail.com>
2023-03-29 20:33:33 -0400

    created branch trial modes for Q1PA {struct typedef such that skips labels}

--- Question 1/Part A/part_a_on_altt.c
+++ Question 1/Part A/part_a_on_altt.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
 /*  Defining program macros */
@@ -13,19 +14,18 @@
 
 FILE* file_o(void);
 void file_c(FILE* file);
-int rlines(FILE* file, datatypes array[LINE_SIZE]);
-datatypes data_points(FILE* file, datatypes array[LINE_SIZE]);
 
 /*  Initializing struct datatypes for CSV data  */
 
 typedef struct
 {
-    int year;
+    //  int year;
+    char year[10];
     char province[35];
     char age_group[15];
     char sex[10];
-    //  char values[5];
-    int values;
+    //  int values;
+    char values[10];
 }datatypes;
 
 void main(void)
@@ -33,19 +33,57 @@ void main(void)
     FILE* f = file_o();
     
     char string[ARRAY_SIZE];
-    while(!feof(f))
+
+    int line = 0;
+    int data_count = 0;
+
+    datatypes data_set[LINE_SIZE];  /*  Struct array to store values from CSV   */
+
+    int init = 0;
+    while (fgets(string, ARRAY_SIZE, f))
     {
-        fgets(string, ARRAY_SIZE,f);
-        printf("%s\n", string);
+        line++;
+        data_count = 0;
+        if (line == 1)
+        {
+            continue;   /*  Parsing the first line as line 1 encompasses labels and headers which are of no relavance   */
+        }
+        char* data_value = strtok(string, ","); /*  Seperating the line string into subsequent smaller string based on comma seperation */
+        while (data_count)
+        {
+            if (data_count == 0)
+            {
+                strcpy(data_set[init].year, data_value);
+            }
+            if (data_count == 1)
+            {
+                strcpy(data_set[init].province, data_value);
+            }
+            if (data_count == 3)
+            {
+                strcpy(data_set[init].age_group, data_value);
+            }
+            if (data_count == 4)
+            {
+                strcpy(data_set[init].sex, data_value);
+            }
+            if (data_count == 13)
+            {
+                strcpy(data_set[init].values, data_value);
+            }
+            data_value = strtok(NULL, ",");
+            data_count++;
+        }
+        init++;
     }
-    
-    datatypes data_set[LINE_SIZE];
-
-    int line_read = rlines(f, data_set);
-
-    data_set[LINE_SIZE] = data_points(f, data_set);
 
     file_c(f);
+
+    for (int i = 0; i <= line; i++)
+    {
+        printf("%s %s %s %s %s\n", data_set[i].year, data_set[i].province, data_set[i].age_group, data_set[i].sex, data_set[i].values);
+    }
+
 }
 
 /*  Defining all subsequent UDF's utilized in the program   */
@@ -69,49 +107,3 @@ void file_c(FILE* file) /*  UDF for file closing    */
     fclose(file);   /*  Closing the already opened file */
 }
 
-int rlines(FILE* file, datatypes array[LINE_SIZE])  /*  UDF for determining the number of lines read from the CSV files */
-{
-    int lines = 0;
-    int data_values = 0;
-    int temp;
-    while(!feof(file))
-    {
-        if (data_values > 1)
-        {
-            data_values = fscanf(file, "%d, %35[^,], %15[^,], %8[^,], %d", &array[lines].year, array[lines].province, array[lines].age_group, array[lines].sex, &array[lines].values);
-        }
-        else if (data_values < 1)
-        {
-            data_values = fscanf(file, "", temp);
-        }
-    }
-    return lines;
-}
-
-datatypes data_points(FILE* file, datatypes array[LINE_SIZE])   /*  UDF for scanning and addressing the CSV data points into a set of pre-defined struct arrays */
-{
-    int lines = 0;
-    int data_values = 0;
-    int temp;
-    while(!feof(file))
-    {
-        if (data_values > 1)
-        {
-            data_values = fscanf(file, "%d, %35[^,], %15[^,], %8[^,], %d", &array[lines].year, array[lines].province, array[lines].age_group, array[lines].sex, &array[lines].values);
-        }
-        else if (data_values < 1)
-        {
-            data_values = fscanf(file, "", temp);
-        }
-    }
-    return array[LINE_SIZE];
-}
-
-/*typedef struct datatypes
-{
-    int year;
-    char province[50];
-    char age_group[25];
-    char sex[10];
-    char values[5];
-};*/
