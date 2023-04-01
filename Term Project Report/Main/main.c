eaf022948f31efffecd21cb075b658db9e38b2db
Anonymous-User-42 <mohammedsayeedahamad@gmail.com>
2023-04-01 12:27:33 -0400

    Completed Q1PA and commiting to main

--- Term Project Report/Main/main.c
+++ Term Project Report/Main/main.c
@@ -18,14 +18,14 @@ void file_c(FILE* file);
 
 /*  Initializing struct datatypes for CSV data  */
 
-typedef struct {
+typedef struct {    /*  Struct to store every Parameter in a line as an array of tokens with respect to their induvidual fields */
   char year[10];
   char province[35];
   char age_group[20];
   char sex[10];
   char values[10];
   char temp_str[10];
-} datatypes;
+} datatypes;    /*  Struct DataType Variable name defined as "datatypes"    */
 
 void main(void)
 {
@@ -82,19 +82,125 @@ void main(void)
 
     file_c(f);  /*  Terminating File Operations */
 
+    /*          Question 1          */printf("|--------------------Question 1--------------------|\n");
+
+    /*  Initializing Sum & Iterating Counter Variables for Averages Calculation */
+
+    //  float sum_province - Variable to store the sum of the Value data Points subsequently in every iteration
+    //  province_iterator_counter - Variable to calculate the number of iterations performed in the for loop
+
+    /*  Federal Variables   */
+
     float sum_federal = 0;
     int federal_iterator_counter = 0;
 
+    /*  Quebec Variables    */
+
+    float sum_quebec = 0;
+    int quebec_iterator_counter = 0;
+
+    /*  Ontario Variables   */
+
+    float sum_ontario = 0;
+    int ontario_iterator_counter = 0;
+
+    /*  Alberta Variables   */
+
+    float sum_alberta = 0;
+    int alberta_iterator_counter = 0;
+
+    /*  British Columbia    */
+
+    float sum_british_columbia = 0;
+    int british_columbia_iterator_counter = 0;
+
+    /*  Federal Average Calculator  */
+
     for (int i = 2; i < 44; i++, federal_iterator_counter++)
     {
-      char add_val_f[10];
-      char stg_val_f[10];
-      strcpy(add_val_f, data_set[i].values);
-      strcpy(stg_val_f, &add_val_f[1]);
-      double values_federal = atof(stg_val_f);
-      //printf("%.2lf\n",values_federal);
-      sum_federal =+ values_federal;
-    } double avg_federal = (sum_federal) / (federal_iterator_counter); printf("Federal Average: %.2lf\n", avg_federal);
+      char add_val_f[10];   /*  Initializing char variable to copy char pointer to char variable type   */
+      char stg_val_f[10];   /*  Initializing char variable to store char to char recieved from char pointer */
+      strcpy(add_val_f, data_set[i].values);    /*  Copying to char variable from char pointer variable */
+      strcpy(stg_val_f, &add_val_f[1]); /*  Storing the char variable in another char variable to manipulate string literals    */
+      double values_federal = atof(stg_val_f);  /*  Converting the stored char variable to a float type varaible data type for mathematical computational maniupulation */
+      if (values_federal == 0)
+      {
+        federal_iterator_counter--; /*  Fail-Safe Mechanism for not counting the iterations in the iterating counter factor if the condition is met [CONDITION IS ONLY SATISFIED IF THE atof FUNCTION RETURN 0, IFF THE Var(char) = NULL]   */
+      }
+      //printf("%.2lf\n",values_federal);   /*  Fail-Safe in Testing phase to verify succinctity of the values being read from atof function    */
+      sum_federal += values_federal;    /*  Calculation of Summa function of all data points being read that are not NULL   */
+    } double avg_federal = (sum_federal) / (federal_iterator_counter); printf("Federal Average: %.2lf\n", avg_federal); /*  Calculation of the Average function from the previous Summa function and iterator counter function as inputs    */
+
+    /*  Quebec Average Calculator   */
+
+    for (int i = 44; i < 86; i++, quebec_iterator_counter++)
+    {
+      char add_val_f[10];   /*  Initializing char variable to copy char pointer to char variable type   */
+      char stg_val_f[10];   /*  Initializing char variable to store char to char recieved from char pointer */
+      strcpy(add_val_f, data_set[i].values);    /*  Copying to char variable from char pointer variable */
+      strcpy(stg_val_f, &add_val_f[1]); /*  Storing the char variable in another char variable to manipulate string literals    */
+      double values_quebec = atof(stg_val_f);  /*  Converting the stored char variable to a float type varaible data type for mathematical computational maniupulation */
+      if (values_quebec == 0)
+      {
+        quebec_iterator_counter--; /*  Fail-Safe Mechanism for not counting the iterations in the iterating counter factor if the condition is met [CONDITION IS ONLY SATISFIED IF THE atof FUNCTION RETURN 0, IFF THE Var(char) = NULL]   */
+      }
+      //printf("%.2lf\n",values_quebec);   /*  Fail-Safe in Testing phase to verify succinctity of the values being read from atof function    */
+      sum_quebec += values_quebec;    /*  Calculation of Summa function of all data points being read that are not NULL   */
+    } double avg_quebec = (sum_quebec) / (quebec_iterator_counter); printf("Quebec Average: %.2lf\n", avg_quebec);
+
+    /*  Ontario Average Calculator  */
+
+    for (int i = 86; i < 128; i++, ontario_iterator_counter++)
+    {
+      char add_val_f[10];   /*  Initializing char variable to copy char pointer to char variable type   */
+      char stg_val_f[10];   /*  Initializing char variable to store char to char recieved from char pointer */
+      strcpy(add_val_f, data_set[i].values);    /*  Copying to char variable from char pointer variable */
+      strcpy(stg_val_f, &add_val_f[1]); /*  Storing the char variable in another char variable to manipulate string literals    */
+      double values_ontario = atof(stg_val_f);  /*  Converting the stored char variable to a float type varaible data type for mathematical computational maniupulation */
+      if (sum_ontario == 0)
+      {
+        ontario_iterator_counter--; /*  Fail-Safe Mechanism for not counting the iterations in the iterating counter factor if the condition is met [CONDITION IS ONLY SATISFIED IF THE atof FUNCTION RETURN 0, IFF THE Var(char) = NULL]   */
+      }
+      //printf("%.2lf\n",values_ontario);   /*  Fail-Safe in Testing phase to verify succinctity of the values being read from atof function    */
+      sum_ontario += values_ontario;    /*  Calculation of Summa function of all data points being read that are not NULL   */
+    } double avg_ontario = (sum_ontario) / (ontario_iterator_counter); printf("Ontario Average: %.2lf\n", avg_ontario);
+
+    /*  Alberta Average Calculator  */
+
+    for (int i = 128; i < 170; i++, alberta_iterator_counter++)
+    {
+      char add_val_f[10];   /*  Initializing char variable to copy char pointer to char variable type   */
+      char stg_val_f[10];   /*  Initializing char variable to store char to char recieved from char pointer */
+      strcpy(add_val_f, data_set[i].values);    /*  Copying to char variable from char pointer variable */
+      strcpy(stg_val_f, &add_val_f[1]); /*  Storing the char variable in another char variable to manipulate string literals    */
+      double values_alberta = atof(stg_val_f);  /*  Converting the stored char variable to a float type varaible data type for mathematical computational maniupulation */
+      if (values_alberta == 0)
+      {
+        alberta_iterator_counter--; /*  Fail-Safe Mechanism for not counting the iterations in the iterating counter factor if the condition is met [CONDITION IS ONLY SATISFIED IF THE atof FUNCTION RETURN 0, IFF THE Var(char) = NULL]   */
+      }
+      //printf("%.2lf\n",values_alberta);   /*  Fail-Safe in Testing phase to verify succinctity of the values being read from atof function    */
+      sum_alberta += values_alberta;    /*  Calculation of Summa function of all data points being read that are not NULL   */
+    } double avg_alberta = (sum_alberta) / (alberta_iterator_counter); printf("Alberta Average: %.2lf\n", avg_alberta);
+
+    /*  British Columbia Average Calculator */
+
+    for (int i = 170; i < 212; i++, british_columbia_iterator_counter++)
+    {
+      char add_val_f[10];   /*  Initializing char variable to copy char pointer to char variable type   */
+      char stg_val_f[10];   /*  Initializing char variable to store char to char recieved from char pointer */
+      strcpy(add_val_f, data_set[i].values);    /*  Copying to char variable from char pointer variable */
+      strcpy(stg_val_f, &add_val_f[1]); /*  Storing the char variable in another char variable to manipulate string literals    */
+      double values_british_columbia = atof(stg_val_f);  /*  Converting the stored char variable to a float type varaible data type for mathematical computational maniupulation */
+      if (values_british_columbia == 0)
+      {
+        british_columbia_iterator_counter--; /*  Fail-Safe Mechanism for not counting the iterations in the iterating counter factor if the condition is met [CONDITION IS ONLY SATISFIED IF THE atof FUNCTION RETURN 0, IFF THE Var(char) = NULL]   */
+      }
+      //printf("%.2lf\n",values_british_columbia);   /*  Fail-Safe in Testing phase to verify succinctity of the values being read from atof function    */
+      sum_british_columbia += values_british_columbia;    /*  Calculation of Summa function of all data points being read that are not NULL   */
+    } double avg_british_columbia = (sum_british_columbia) / (british_columbia_iterator_counter); printf("British Columbia Average: %.2lf\n", avg_british_columbia);
+
+
+
 }
 
 /*  Defining all subsequent UDF's utilized in the program   */
