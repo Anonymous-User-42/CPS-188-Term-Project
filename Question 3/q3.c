38ceea2c27374c713c29eba78f4db9c5a69a19cf
ThatTreeGuy25 <127777351+ThatTreeGuy25@users.noreply.github.com>
2023-03-31 23:42:39 -0400

    Update q3.c

--- Question 3/q3.c
+++ Question 3/q3.c
@@ -1,34 +1,83 @@
-double national_average = ((sum_quebec / count_quebec) + (sum_ontario / count_ontario) + (sum_alberta / count_alberta) + (sum_british_columbia / count_british_columbia)) / 4;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
- 
- printf("The National Average of Diabetes is: %lf\n", national_average); 
- 
- if ((sum_quebec / count_quebec) < national_average) {
-	 printf("The average in Quebec is lower than the national average\n"); //check if averages for provinces are lower or higher than the national average 
- }
- else {
-	printf("The average in Quebec is higher than the national average\n");
-}
+//PARSE THE CSV FILE AND STORE INTO ARRAY of Structs
+
+typedef struct diabetes
+{
+	char REF_DATE[100];
+	char GEO[100];
+	char Age_Group[100];
+	char Sex[100];
+	char VALUE[100];
+}	dict;
+
+int main()
+{
+	FILE *fp = fopen("statscan_diabetes.csv", "r"); //open in read mode 
 	
- if ((sum_ontario / count_ontario) < national_average) {
-	printf("The average in Ontario is lower than the national average\n");
-}
-
- else  {
-	printf("The average in Ontario is higher than the national average\n");
-}
-
- if ((sum_alberta / count_alberta) < national_average) {
-	printf("The average in Alberta is lower than the national average\n");
-}
- if ((sum_alberta / count_alberta) > national_average) {
-	printf("The average in Alberta is higher than the national average\n");
-}
+	if(!fp	) 
+	{
+		printf("Cannot Open File");
+		return 1;
+	}
 	
- if ((sum_british_columbia / count_british_columbia) < national_average) {
-	printf("The average in British Columbia is higher than the national average\n");
+	char buff[1024]; 
+	int row_count = 0;
+	int field_count = 0;
+	
+	dict values[999];
+	
+	int i = 0;
+	while (fgets(buff, 1024, fp))
+	{
+		field_count = 0;
+		row_count++;
+		if(row_count == 1)
+		continue; 
+		
+		char *field = strtok(buff, ",");
+		while(field)
+	{ 
+		if(field_count == 0)
+			strcpy(values[i].REF_DATE, field);	
+		if(field_count == 1)
+			strcpy(values[i].GEO, field);
+		if(field_count == 3)
+			strcpy(values[i].Age_Group, field);
+		if(field_count == 4)
+			strcpy(values[i].Sex, field);
+		if(field_count == 13)
+			strcpy(values[i].VALUE, field);
+			
+		field = strtok(NULL, ",");
+		field_count++;
+	}
+	i++;
 }
- else  {
-	printf("The average in British Columbia is higher than the national average\n");
+fclose(fp);
+
+printValues(values);
+	
+
+		
+	return 0;
 }
+
+void printValues(dict values[])
+{
+	
+	for(int i=0; i<42; i++)
+	{
+		printf("Province->%s VALUE->%s\n" ,values[i].GEO, values[i].VALUE);
+	}
+	
+	double sum = 0;
+	
+	for(int i =0; i<42; i++)
+	{
+		sum += atof(values[i].VALUE);
+	}
+	printf("Sum: %lf\n", sum);
 }
